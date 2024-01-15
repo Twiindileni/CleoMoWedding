a62f532779bb04d1c07e9be54f8887d246332792
Twiindileni Tomas <150262434+Twiindileni@users.noreply.github.com>
2024-01-15 18:20:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const acceptButton = document.getElementById("acceptButton");
+// script.js
 
-    acceptButton.addEventListener("click", function() {
-        // Redirect to another page when the button is clicked
-        window.location.href = "Accept.html";
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    const acceptButton1 = document.getElementById("acceptButton1");
+    const acceptButton2 = document.getElementById("acceptButton2");
+
+    if (acceptButton1) {
+        acceptButton1.addEventListener("click", function() {
+            // Redirect to the next page for the first HTML
+            window.location.href = "list.html";
+        });
+    }
+
+    if (acceptButton2) {
+        acceptButton2.addEventListener("click", function() {
+            // Redirect to the next page for the second HTML
+            window.location.href = "Accept.html";
+        });
+    }
 });
